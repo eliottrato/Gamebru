1f7cb7c65915baf2724bdeec0cb67de26f9574ef
eliottrato <91064295+eliottrato@users.noreply.github.com>
2021-10-11 18:29:09 +0400

    Update game.js

--- game.js
+++ game.js
@@ -3,3 +3,20 @@ script src="game.js"></script>
  Warning - JavaScript is Disabled.
  For full functionality of this page, it is necessary to enable JavaScript.
 </noscript>
+unction Bear() {
+ this.dBear = 100;
+ this.htmlElement = document.getElementById("bear");
+ this.id = this.htmlElement.id;
+ this.x = this.htmlElement.offsetLeft;
+ this.y = this.htmlElement.offsetTop;
+ this.move = function(xDir, yDir) {
+ this.x += this.dBear * xDir;
+ this.y += this.dBear * yDir;
+ this.display();
+ };
+ this.display = function() {
+ this.htmlElement.style.left = this.x + "px";
+ this.htmlElement.style.top = this.y + "px";
+ this.htmlElement.style.display = "block";
+ };
+}
