50e717bb40c128d584c8e5a47c9f169f78f41ee4
eliottrato <91064295+eliottrato@users.noreply.github.com>
2021-10-12 10:25:58 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,59 @@
+<script>
+ var bear;
+ </script>
+<script src="game.js"></script>
+<noscript>
+ Warning - JavaScript is Disabled.
+ For full functionality of this page, it is necessary to enable JavaScript.
+</noscript>
+body onload="start();">
+function Bear() {
+ this.dBear = 100;
+ this.htmlElement = document.getElementById("bear");
+ this.id = this.htmlElement.id;
+ this.x = this.htmlElement.offsetLeft;
+ this.y = this.htmlElement.offsetTop;
+ this.move = function(xDir, yDir) {
+ this.x += this.dBear * xDir;
+ this.y += this.dBear * yDir;
+ this.display();
+ };
+ this.display = function() {
+ this.htmlElement.style.left = this.x + "px";
+ this.htmlElement.style.top = this.y + "px";
+ this.htmlElement.style.display = "block";
+ };
+ function start() {
+ //create bear
+ bear = new Bear();
+}
+ // Handle keyboad events 
+// to move the bear
+function moveBear(e) {
+ //codes of the four keys
+ const KEYUP = 38;
+ const KEYDOWN = 40;
+ const KEYLEFT = 37;
+ const KEYRIGHT = 39;
+ if (e.keyCode == KEYRIGHT) {
+ bear.move(1, 0)
+ } // right key
+ if (e.keyCode == KEYLEFT) {
+ bear.move(-1, 0)
+ } // left key
+ if (e.keyCode == KEYUP) {
+ bear.move(0, -1)
+ } // up key
+ if (e.keyCode == KEYDOWN) {
+ bear.move(0, 1)
+ } // down key
+}
+ function start() {
+ //create bear
+ bear = new Bear();
+ // Add an event listener to the keypress event.
+ document.addEventListener("keydown", moveBear, false)
+}
 class Bee {
  constructor(beeNumber) {
  //the HTML element corresponding to the IMG of the bee
